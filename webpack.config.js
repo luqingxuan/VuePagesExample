a81cbf044675839f5124abf462161ea4c05ff7a8
qingjie.cheng <qingjie.cheng@datayes.com>
2016-06-02 20:21:17 +0800

    Change-Id: I843aaf522e7a70557bff235f4d9b6cb01351647a

--- webpack.config.js
+++ webpack.config.js
@@ -205,7 +205,7 @@ module.exports = {
 		root : [ path.join(__dirname, 'src'),
 				path.join(__dirname, 'node_modules') ],
 		// 自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
-		extensions : [ '', '.js', '.vue', 'jsx', '.json', '.scss' ],
+		extensions : [ '', '.js', '.vue', '.json', '.scss' ],
 		// 模块别名定义，方便后续直接引用别名，无须多写长长的地址
 		alias : {}
 	},
