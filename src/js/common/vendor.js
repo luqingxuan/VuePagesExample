819684f32a8b1c6473b1cf52a025ec36fa2c3573
qingjie.cheng <qingjie.cheng@datayes.com>
2016-06-03 17:08:56 +0800

    Change-Id: I768d8f4d1a4a4ddd724da9b9b70f7840b4ab270a

--- src/js/common/vendor.js
+++ src/js/common/vendor.js
@@ -1,3 +1,12 @@
+//ES6 Promise Object fix
+require('es6-promise').polyfill();
+
+// ES6 Fetch Object fix
+require('fetch-polyfill');
+
+// ES6 Object.assign fix
+require('core-js/es6/object');
+
 window.$ = $;
 
 // 解决严格模式下，没有声明就使用的报错
